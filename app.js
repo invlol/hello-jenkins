72ad8497fbe8b0128d426401cc78caf7afa1c4ad
Antonio <antonio.alarcon@outlook.com>
2015-12-29 12:38:22 -0500

    cambios

--- app.js
+++ app.js
@@ -27,3 +27,17 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 5000);
  
 module.exports = app;
+
+/*
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT || 5000);
+
+module.exports = app;
+*/
